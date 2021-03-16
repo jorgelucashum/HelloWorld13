35a3059e41e32389b7c7305804af8f00d2373395
jorgeucas lucas <jorge_dofus1@hotmail.com>
2021-03-16 16:22:22 -0300

    Add project files.

---
+++ HelloWorld13/Produto.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace HelloWorld13
+{
+    class Produto
+    {
+        private string _nome; 
+        private int _quantidade;
+        private double _preco;
+
+        public Produto(string nome, double preco, int quantidade) 
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+
+        public Produto() { } // Sobrecarga para funcionar o construtor padrão e sintaxe alternativa (Só é necessário quando se declara algum construtor)
+
+        // Properties:
+        public string Nome // Em vez de chamar o método get/set direto em outra classe para privar os atributos, usa-se a propertie para simplificar a sintaxe, como se fosse um atributo.
+        {
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1) {
+                    _nome = value;                     }
+                
+            }
+        }
+
+        public double Preco // propertie retornando só get
+        {
+            get { return _preco; }
+        }
+
+        public int Quantidade
+        {
+            get { return _quantidade; }
+        }
+        //
+
+
+        //Get:
+        public string GetNome() // método Get para a variável nome ser obtida em outras classes
+        {
+            return _nome;
+        }
+        //Set:
+        public void SetNome(string nome)
+        {
+            // usando um método como 'Set' é possível colocar uma lógica também.
+            if (nome != null && nome.Length > 1)
+            { // 'nome.Length' é usado pra contar o tamanho do string 'nome' (nesse caso maior que 1 letra)
+                _nome = nome;
+            }
+        }
+
+        public double GetPreco() // usando somente o 'Get' para que não posso ser feita alterações fora da classe, somente retornando o valor.
+        {
+            return _preco;
+        }
+
+        public int GetQuantidade()
+        {
+            return _quantidade;
+        }
+
+        public double ValorTotalEmEsqtoque()
+        {
+            return _quantidade * _preco;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            _quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            _quantidade -= quantidade;
+        }
+
+        public override string ToString() // método usado para retornar a váriavel 'p1' nas informações em String
+        {
+            return _nome + ", R$:" + _preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + _quantidade + " " +
+                "Unidades, total R$:" + ValorTotalEmEsqtoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ HelloWorld13/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld13
+{
+    class Program
+    {
+        // properties: São definições de métodos encapsulados, porém expondo uma sintaxe similiar à de atributos e não de métodos.
+        static void Main(string[] args)
+        {
+            Produto p1 = new Produto("TV", 500.00, 10);
+
+            p1.Nome = "TV 4K"; // usando  o método 'set' em forma de propertie.
+
+            Console.WriteLine(p1.Nome);
+            Console.WriteLine(p1.Preco);
+            Console.WriteLine(p1.Quantidade);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
